5455dc1788cf671d907007d7e9c3df84c2040361
Oksana Nedilko <nedilko7b@gmail.com>
2022-05-16 12:23:11 +0300

    lab6-finish

--- books-lab6/index.js
+++ books-lab6/index.js
@@ -5,29 +5,6 @@ const morgan = require('morgan')
 require('dotenv').config()
 const Book = require('./models/book')
 
-let persons = [
-  { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-  },
-  { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-  }
-]
-
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
